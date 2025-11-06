d88b7f0e3f258c979c18097890e58e7f99fb4a4f
Imran Ali Namazi <imran@amadeusweb.com>
2025-11-06 20:44:22 +0530

    lgittoramiw

--- cms.php
+++ cms.php
@@ -1,4 +1,21 @@
 <?php
 variables([
 	'link-to-sub-node-home' => true,
+	'humanize'
 ]);
+
+function site_before_render() {
+	$section = variable('section');
+	if ($section != 'ideas') return;
+
+	$node = variable('node');
+	if ($section == $node) return;
+
+	DEFINE('NODEPATH', SITEPATH . '/' . variable('section') . '/' . $node);
+	variables([
+		'nodeSiteName' => humanize($node),
+		'nodeSafeName' => $node,
+		'submenu-at-node' => true,
+		'nodes-have-files' => true,
+	]);
+}
--- with-ai/2025-10/01-to-05/home.md
+++
@@ -1,3 +0,0 @@
-## %fileName%
-
-%ai-citations-corecodesnippet%
